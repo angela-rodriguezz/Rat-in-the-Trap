dedfae7baaf629d8964a87e9dd77aa1a6d2f577f
Angela <amr25@berkeley.edu>
2023-08-28 20:01:01 -0500

    added animation + fixed timer issue

--- Rat_in_The_Trap-FINAL/Assets/Scripts/SelectionScreen.cs
+++ Rat_in_The_Trap-FINAL/Assets/Scripts/SelectionScreen.cs
@@ -9,7 +9,7 @@ public class SelectionScreen : MonoBehaviour
     public ChooseOptionController label; 
     public PlayDialogue gameController; // used to play the next scene that corresponds to the choice the player made
     private RectTransform rectTransform; // used to alter the positions of the timer
-    private Animator animator; // animator to show or hide the button options
+    public Animator animator; // animator to show or hide the button options
     public Button btn1; // button one choice
     public Button btn2; // button two choice
     public Button btn3; // button three choice
@@ -23,31 +23,34 @@ public class SelectionScreen : MonoBehaviour
     private float timer = 0.0f;
     private bool activeTimer = false;
     public float shakeAmount = 0.075f; // This is the amount of text shake you want to apply
-    public float shakeSpeed = 50f; // This is the speed of the text shake
+    public float shakeSpeed = 20f; // This is the speed of the text shake
     private Vector3 timerOriginalPosition; // This will store the original position of the Text element
 
     
     void Start()
     {
-        animator = GetComponent<Animator>();
         rectTransform = GetComponent<RectTransform>();
         timerOriginalPosition = timerText.transform.position;
     }
 
     void Update() 
     {
-        // If timer is active
-        timerText.gameObject.SetActive(activeTimer);
-        timerText.text = (15.0f - Mathf.Round(timer * 100.0f) / 100.0f).ToString();
+        if (activeTimer)
+        {
+            // If timer is active
+            timerText.gameObject.SetActive(activeTimer);
+            timerText.text = (15.0f - Mathf.Round(timer * 100.0f) / 100.0f).ToString();
 
-        float shake = Mathf.Sin(Time.time * shakeSpeed) * shakeAmount; // Calculate the shake amount using a sine wave
-        timerText.transform.position = timerOriginalPosition + new Vector3(shake, 0f, 0f); // Apply the shake to the Text element's position
+            float shake = Mathf.Sin(Time.time * shakeSpeed) * shakeAmount; // Calculate the shake amount using a sine wave
+            timerText.transform.position = timerOriginalPosition + new Vector3(shake, 0f, 0f); // Apply the shake to the Text element's position
+        }
 
         // If the button is active 
         if (timer >= 15.0f && btn1.onClick.GetPersistentEventCount() > 0 && btn2.onClick.GetPersistentEventCount() > 0 && btn3.onClick.GetPersistentEventCount() > 0) 
         {
             PerformChoice(2);
         }
+
     }
 
     // adds to the value of the timer if active
@@ -64,14 +67,14 @@ public class SelectionScreen : MonoBehaviour
     // changes the text of the buttons to each of the options
     // starts the timer
     public void SetupChoose(ChooseScene scene)
-    {   
-        //animator.SetTrigger("Show");
+    {
+        activeTimer = true;
+        animator.SetTrigger("Show");
         main = scene;
         buttonChoice1.text = scene.labels[0].text;
         buttonChoice2.text = scene.labels[1].text;
         buttonChoice3.text = scene.labels[2].text;
         timer = 0.0f;
-        activeTimer = true;
     }
     // Plays the scene dialogue that occurs after making the choice
     // hides the previous choices
